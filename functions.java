c80d7c09ea570ac8b2c1c26e1f187c13e47758df
BALARAM <balaramtrishal@BALARAMs-MacBook-Air.local>
2025-06-02 22:27:13 +0530

    JAVA

--- functions.java
+++ functions.java
@@ -1,5 +1,5 @@
 import java.util.*;
-class FunctionsDemo {
+class Functions {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.print("Enter the number of elements: ");
